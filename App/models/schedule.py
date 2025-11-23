5e7297574dbee3efeb104afb02d378bb632aa8db
TwiSteorra <twisteorra@gmail.com>
2025-11-23 16:45:36 -0400

    #8 Implement Schedule Model

---
+++ App/models/schedule.py
@@ -0,0 +1,17 @@
+from App.database import db
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    residents = db.relationship("Resident", backref="schedule", lazy=True)
+
+    def subscribe(self, resident):
+        self.residents.append(resident)
+        
+    def unsubscribe(self, resident):
+        self.residents.remove(resident)
+    
+    def notify_subscribers(self, message):
+        for resident in self.residents:
+            resident.update()
+
